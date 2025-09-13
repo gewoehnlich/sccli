07e11440f64dbdc1018318e671f78f7b9f8a20f9
gewoehnlich <oomyse@gmail.com>
2025-09-13 18:49:01 +0500

    config settings update

--- core/settings.py
+++ core/settings.py
@@ -1,1 +1,89 @@
+import sys
+import yaml
+from typing import Any, Self
+from pydantic import BaseModel, ValidationError
+
 from default_settings.app import AppSettings
+
+
+class Settings():
+    CONFIG_PATH: str = "config.yml"
+
+    _instance:    Self | None = None
+    _initialized: bool        = False
+
+
+    def __new__(
+        cls: type[Self],
+        *args,
+        **kwargs
+    ) -> Self:
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+
+    def __init__(
+        self
+    ) -> None:
+        if self._initialized:
+            return
+
+        super().__init__()
+
+
+    def load(
+        self,
+    ) -> AppSettings:
+        user_config_data: dict[str, Any] = self.read_user_config_data()
+        filtered_config: dict[str, Any] = self.remove_none_values(user_config_data)
+        settings = self.add_default_values(filtered_config)
+        return settings
+
+
+    def read_user_config_data(
+        self,
+    ) -> dict[str, Any]:
+        try:
+            with open(
+                file = self.CONFIG_PATH,
+                mode = "r",
+                encoding = "utf-8"
+            ) as f:
+                user_config_data = yaml.safe_load(f) or {}
+
+        # throw an error
+        except FileNotFoundError:
+            print(f"INFO: '{ self.CONFIG_PATH }' not found. Using default settings.")
+            user_config_data = {}
+
+        return user_config_data
+
+
+    def remove_none_values(
+        self,
+        config: dict[str, Any] | None,
+    ) -> dict[str, Any] | None:
+        if isinstance(config, dict):
+            return {
+                key: self.remove_none_values(value)
+                for key, value in config.items()
+                if value is not None
+            }
+
+        return config
+
+
+    def add_default_values(
+        self,
+        config: dict[str, Any],
+    ) -> AppSettings:
+        try:
+            settings = AppSettings.model_validate(config)
+
+        except ValidationError as e:
+            print(f"ERROR: Invalid configuration in '{ CONFIG_PATH }':\n{e}", file=sys.stderr)
+            sys.exit(1)
+
+        return settings
--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import sys
-import yaml
 from dependency_injector.wiring import Provide
-from pydantic import ValidationError
 
 from core.di_container import DiContainer
-from default_settings.app import AppSettings
+from core.settings import Settings
 # from core.shell import Shell
-
-
-def remove_none_values(config):
-    """Recursively remove keys with None values from a dictionary."""
-    if isinstance(config, dict):
-        return {
-            key: remove_none_values(value)
-            for key, value in config.items()
-            if value is not None
-        }
-    if isinstance(config, list):
-        return [remove_none_values(item) for item in config]
-    return config
 
 
 def main(di_container: DiContainer = Provide[DiContainer]) -> None:
@@ -27,38 +11,14 @@ def main(di_container: DiContainer = Provide[DiContainer]) -> None:
 
 
 if __name__ == "__main__":
-    CONFIG_PATH: str = "config.yml"
+    settings = Settings().load()
+    print(settings)
 
-    try:
-        with open(
-            file = CONFIG_PATH,
-            mode = "r",
-            encoding = "utf-8"
-        ) as f:
-            user_config_data = yaml.safe_load(f) or {}
-
-    except FileNotFoundError:
-        print(f"INFO: '{ CONFIG_PATH }' not found. Using default settings.")
-        user_config_data = {}
-
-    # Filter out None values so Pydantic can apply defaults
-    filtered_config = remove_none_values(user_config_data)
-
-    try:
-        settings = AppSettings.model_validate(filtered_config)
-        print(settings)
-
-    except ValidationError as e:
-        print(f"ERROR: Invalid configuration in '{ CONFIG_PATH }':\n{e}", file=sys.stderr)
-        sys.exit(1)
-
-    sys.exit(1)
-
-    di_container: DiContainer = DiContainer()
-
-    di_container.config.from_pydantic(settings)
-
-    di_container.db().initialize_tables()
-    di_container.wire(modules=[__name__])
-
-    main()
+    # di_container: DiContainer = DiContainer()
+    #
+    # di_container.config.from_pydantic(settings)
+    #
+    # di_container.db().initialize_tables()
+    # di_container.wire(modules=[__name__])
+    #
+    # main()
