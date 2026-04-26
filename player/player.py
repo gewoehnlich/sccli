35816ac3ae8185462df467464fdc621014769c61
gewoehnlich <oomyse@gmail.com>
2026-04-27 00:06:45 +0500

    basic header and footer

--- player/player.py
+++ player/player.py
@@ -1,5 +1,6 @@
 from typing import Any
 from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
 
 from core.logger import Logger
 from player.widgets.track_list import TrackList
@@ -19,9 +20,18 @@ class Player(App):
         self.logger = logger
 
     def compose(self) -> ComposeResult:
+        yield Header(
+            show_clock=True,
+            name="sccli",
+            id="header"
+        )
         yield TrackList(
             track_repository=self.track_repository,
             logger=self.logger,
+        )
+        yield Footer(
+            name="sccli",
+            id="footer"
         )
 
     def on_mount(self) -> None:
